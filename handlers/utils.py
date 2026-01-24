cf87659c9add01917081fd0972793acd078a8c75
Matthew Becker <mateo62009mp@gmail.com>
2026-01-24 02:38:40 -0300

    Update utils.py

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,32 +1,13 @@
-import time
 from telebot.types import Message
 from config import bot
 
 @bot.message_handler(commands=['clear', 'clean'])
 def cmd_clear(message: Message):
-    """
-    Deletes the last 30 messages in the chat to clean the view.
-    Note: Can only delete messages less than 48 hours old.
-    """
     chat_id = message.chat.id
-    current_msg_id = message.message_id
-    
-    status_msg = bot.send_message(chat_id, "**Cleaning chat...**", parse_mode="Markdown")
-    
-    # Loop backwards to delete messages
-    # We try to delete the last 30 messages (including the /clear or /clean command :D)
-    for i in range(current_msg_id, current_msg_id - 30, -1): # <--- Modify the '30' Thing to delete more Messages.
-        try:
-            bot.delete_message(chat_id, i)
-        except Exception:
-            # If the message is too old or doesn't exist, skip it.
-            continue
-
-    # "Cleaning..." status message gone at this point.
+    msg_id = message.message_id
     try:
-        bot.delete_message(chat_id, status_msg.message_id)
-    except:
-        pass
-
-    # Send final confirmation
-    bot.send_message(chat_id, "**Chat Cleared!**", parse_mode="Markdown")
+        for i in range(msg_id, msg_id - 30, -1):
+            try: bot.delete_message(chat_id, i)
+            except: continue
+        bot.send_message(chat_id, "🧹 Chat Cleaned")
+    except: pass
